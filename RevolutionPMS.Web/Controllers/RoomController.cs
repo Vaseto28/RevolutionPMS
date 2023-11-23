d8dd05ae1bd43a9e180c2e7cf777fb115866aeae
Vaseto28 <vasilkremenarovv@gmail.com>
2023-11-23 09:18:38 +0200

    Initialized first controller classes (Room, Home)

---
+++ RevolutionPMS.Web/Controllers/RoomController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RevolutionPMS.Web.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+    }
+}
+
