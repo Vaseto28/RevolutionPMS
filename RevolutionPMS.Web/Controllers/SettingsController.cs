b7124c514a29de54cf260931e7f03caa61f192cf
Vaseto28 <vasilkremenarovv@gmail.com>
2023-11-29 12:01:14 +0200

    Created SettingsController class

---
+++ RevolutionPMS.Web/Controllers/SettingsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RevolutionPMS.Web.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
+
