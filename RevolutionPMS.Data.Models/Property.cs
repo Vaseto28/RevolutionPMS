27b9720ababc180a6533cb8e9420744ee7c6c5ec
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-04 11:04:08 +0200

    Created Property entity model

---
+++ RevolutionPMS.Data.Models/Property.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static RevolutionPMS.Common.EntitiesValidationsAndConstraints.Property;
+
+namespace RevolutionPMS.Data.Models.DataModels;
+
+public class Property
+{
+	public Property()
+	{
+		this.Id = Guid.NewGuid();
+	}
+
+	[Key]
+	public Guid Id { get; set; }
+
+	[Required]
+	[MaxLength(NameMaxLength, ErrorMessage = NameTooLongErrorMessage)]
+	public string Name { get; set; } = null!;
+
+	[Required]
+	[MaxLength(LocationMaxLength, ErrorMessage = LocationTooLongErrorMessage)]
+	public string Location { get; set; } = null!;
+
+	public int RoomCapacity { get; set; }
+
+	[Required]
+	public string MarketingEmail { get; set; } = null!;
+
+	[Required]
+	public string Website { get; set; } = null!;
+
+	[Required]
+	public Guid CompanyInvoiceDataId { get; set; }
+
+	public virtual CompanyInvoiceData CompanyInvoiceData { get; set; } = null!;
+}
+
