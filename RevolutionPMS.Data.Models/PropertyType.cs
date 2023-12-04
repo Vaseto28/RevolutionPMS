aa1742aaa7f547957fe284e5a3fb726cc4a27ad5
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-04 11:03:52 +0200

    Created PropertyType model

---
+++ RevolutionPMS.Data.Models/PropertyType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static RevolutionPMS.Common.EntitiesValidationsAndConstraints.PropertyType;
+
+namespace RevolutionPMS.Data.Data.DataModels;
+
+public class PropertyType
+{
+	[Key]
+	public Guid Id { get; set; }
+
+	[Required]
+	[MaxLength(NameMaxLength, ErrorMessage = NameTooLongErrorMessage)]
+	public string Name { get; set; } = null!;
+
+	[Required]
+	public int BedsCount { get; set; }
+}
+
