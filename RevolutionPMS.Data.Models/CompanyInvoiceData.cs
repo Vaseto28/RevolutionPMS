70d1b9b1217e1134445f14a92aef1d9a12264ebe
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-04 11:04:23 +0200

    Created CompanyInvoiceData entity model

---
+++ RevolutionPMS.Data.Models/CompanyInvoiceData.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RevolutionPMS.Data.Models.DataModels;
+
+public class CompanyInvoiceData
+{
+	public CompanyInvoiceData()
+	{
+		this.Id = Guid.NewGuid();
+	}
+
+	[Key]
+	public Guid Id { get; set; }
+
+	[Required]
+	[MaxLength(60, ErrorMessage = "The company name is too long!")]
+	public string Name { get; set; } = null!;
+
+	[Required]
+	[MaxLength(11, ErrorMessage = "Invalid VAT number!")]
+	public string VATNumber { get; set; } = null!;
+
+	[Required]
+	[MaxLength(9, ErrorMessage = "Invalid Bulstat / EIC!")]
+	public string Bulstat { get; set; } = null!;
+
+	[Required]
+    [MaxLength(30, ErrorMessage = "The address is too long!")]
+    public string Country { get; set; } = null!;
+
+	[Required]
+	[MaxLength(100, ErrorMessage = "The address is too long!")]
+	public string Address { get; set; } = null!;
+
+	[Required]
+	[MaxLength(50, ErrorMessage = "The city name is too long!")]
+	public string City { get; set; } = null!;
+
+    [Required]
+    [MaxLength(50, ErrorMessage = "The city name is too long!")]
+    public string Region { get; set; } = null!;
+
+	[Required]
+	[MaxLength(4, ErrorMessage = "Invalid post code!")]
+	public string PostCode { get; set; } = null!;
+
+	[Required]
+	[MaxLength(40, ErrorMessage = "The name of the accountable person is too long!. Fill in only first and last name.")]
+	public string AccountablePerson { get; set; } = null!;
+}
+
