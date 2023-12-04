ca89d39b31096f07db54503e58e37d1b1767a9ca
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-04 11:04:50 +0200

    Created EntitiesValidationsConstraints class to hold messages and constants

---
+++ RevolutionPMS.Common/EntitiesValidationsAndConstraints.cs
@@ -0,0 +1,27 @@
+﻿namespace RevolutionPMS.Common;
+
+public static class EntitiesValidationsAndConstraints
+{
+    public static class PropertyType
+    {
+        public const int NameMinLength = 3;
+        public const int NameMaxLength = 50;
+        public const string NameTooLongErrorMessage = "The chosen name is too long!";
+
+        public const int LocationMinLength = 3;
+        public const int LocationMaxLength = 50;
+        public const string LocationTooLongErrorMessage = "The location name is too long!";
+    }
+
+    public static class Property
+	{
+		public const int NameMinLength = 3;
+		public const int NameMaxLength = 50;
+		public const string NameTooLongErrorMessage = "The property name is too long!";
+		
+        public const int LocationMinLength = 3;
+        public const int LocationMaxLength = 50;
+        public const string LocationTooLongErrorMessage = "The location name is too long!";
+    }
+}
+
