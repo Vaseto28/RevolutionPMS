6944eef92a9ad5e4df71217d5f60f12b3765ef48
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-05 22:49:27 +0200

    Created RoomType entity

---
+++ RevolutionPMS.Data.Models/RoomType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static RevolutionPMS.Common.EntitiesValidationsAndConstraints.RoomType;
+
+namespace RevolutionPMS.Data.Models;
+
+public class RoomType
+{
+	[Key]
+	public Guid Id { get; set; }
+
+	[Required]
+	[MaxLength(NameMaxLength, ErrorMessage = NameTooLongErrorMessage)]
+	public string Name { get; set; } = null!;
+
+	[Required]
+	public int BedsCount { get; set; }
+}
+
