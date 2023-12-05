dbad2e123da2885acc7a4b3c3acbcfd23c7ec065
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-05 22:49:38 +0200

    Updated Property type entity

--- RevolutionPMS.Data.Models/PropertyType.cs
+++ RevolutionPMS.Data.Models/PropertyType.cs
@@ -1,18 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using static RevolutionPMS.Common.EntitiesValidationsAndConstraints.PropertyType;
 
-namespace RevolutionPMS.Data.Data.DataModels;
+namespace RevolutionPMS.Data.Models;
 
 public class PropertyType
 {
 	[Key]
-	public Guid Id { get; set; }
+	public int Id { get; init; }
 
 	[Required]
 	[MaxLength(NameMaxLength, ErrorMessage = NameTooLongErrorMessage)]
 	public string Name { get; set; } = null!;
 
-	[Required]
-	public int BedsCount { get; set; }
+	public virtual ICollection<Property> Properties { get; set; } = null!;
 }
 
