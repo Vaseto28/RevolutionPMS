242daeebd584ffa50598b87e128a908ea6a8e5c7
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-07 08:23:49 +0200

    Created BillingTemplate entity model

---
+++ RevolutionPMS.Data.Models/BillingTemplate.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static RevolutionPMS.Common.EntitiesValidationsAndConstraints.BillingTemplate;
+
+namespace RevolutionPMS.Data.Models;
+
+public class BillingTemplate
+{
+	public BillingTemplate()
+	{
+		this.BillingTemplatesBoards = new HashSet<BillingTemplatesBoards>();
+	}
+
+	[Key]
+	public Guid Id { get; set; }
+
+	[Required]
+	[MaxLength(NameMaxLength, ErrorMessage = NameTooLongErrorMessage)]
+	public string Name { get; set; } = null!;
+
+	public decimal Price { get; set; }
+
+	public int Quantity { get; set; }
+
+	public int InitialQuantity { get; set; }
+
+	public decimal TaxPersentage { get; set; }
+
+	[Required]
+	public bool IsPriceChangeable { get; set; }
+
+	public virtual ICollection<BillingTemplatesBoards> BillingTemplatesBoards { get; set; } = null!;
+}
+
