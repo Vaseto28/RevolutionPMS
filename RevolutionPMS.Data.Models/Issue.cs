026b398685bb589ec7c5cbae840e05aec8b02a18
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-07 08:25:13 +0200

    Created Issue entity model

---
+++ RevolutionPMS.Data.Models/Issue.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static RevolutionPMS.Common.EntitiesValidationsAndConstraints.Issue;
+
+namespace RevolutionPMS.Data.Models;
+
+public class Issue
+{
+	public Issue()
+	{
+		this.Rooms = new HashSet<Room>();
+	}
+
+	[Key]
+	public int Id { get; init; }
+
+	[Required]
+	[MaxLength(NameMaxLength, ErrorMessage = NameTooLongErrorMessage)]
+	public string Name { get; set; } = null!;
+
+	[Required]
+	[MaxLength(DescriptionMaxLength, ErrorMessage = DescriptionTooLongErrorMessage)]
+	public string Description { get; set; } = null!;
+
+	[Required]
+	public DateTime StartDate { get; set; }
+
+	[Required]
+	public DateTime EndDate { get; set; }
+
+	public virtual ICollection<Room> Rooms { get; set; } = null!;
+}
+
