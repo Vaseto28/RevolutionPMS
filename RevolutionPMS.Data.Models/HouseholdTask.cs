8e0d615c7f9855e6dfadd587bad4d24bbfbb1e66
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-07 08:25:03 +0200

    Created HouseholdTask model

---
+++ RevolutionPMS.Data.Models/HouseholdTask.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static RevolutionPMS.Common.EntitiesValidationsAndConstraints.HouseholdTask;
+
+namespace RevolutionPMS.Data.Models;
+
+public class HouseholdTask
+{
+	public HouseholdTask()
+	{
+		this.Rooms = new HashSet<Room>();
+	}
+
+	[Key]
+	public int Id { get; init; }
+
+	[Required]
+	[MaxLength(NameMaxLength, ErrorMessage = NameTooLongErrorMessage)]
+	public string Name { get; set; } = null!;
+
+	[Required]
+	[MaxLength(DescriptionMaxLength, ErrorMessage = DescriptionTooLongErrorMessage)]
+	public string Description { get; set; } = null!;
+
+	public virtual ICollection<Room> Rooms { get; set; } = null!;
+}
+
