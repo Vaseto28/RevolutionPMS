597e96440b6f2da12465c01e63d48d87b5f718a4
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-07 08:26:03 +0200

    Created Room entity model

---
+++ RevolutionPMS.Data.Models/Room.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static RevolutionPMS.Common.EntitiesValidationsAndConstraints.Room;
+
+namespace RevolutionPMS.Data.Models;
+
+public class Room
+{
+	[Key]
+	public int Id { get; init; }
+
+	public int Number { get; set; }
+
+	public int Block { get; set; }
+
+	[Required]
+	[MaxLength(DescriptionMaxLength, ErrorMessage = DescriptionTooLongErrorMessage)]
+	public string Description { get; set; } = null!;
+
+	public Guid PropertyId { get; set; }
+
+	public virtual Property Property { get; set; } = null!;
+
+	public int? IssueId { get; set; }
+
+	public virtual Issue? Issue { get; set; }
+
+	public int? HouseholdTaskId { get; set; }
+
+	public virtual HouseholdTask? HouseholdTask { get; set; } = null!;
+}
+
