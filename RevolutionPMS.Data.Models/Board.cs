70a3566a1e0d0b674039a39fbf38c49443b050fc
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-07 08:24:17 +0200

    Created Board model

---
+++ RevolutionPMS.Data.Models/Board.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static RevolutionPMS.Common.EntitiesValidationsAndConstraints.Board;
+
+namespace RevolutionPMS.Data.Models;
+
+public class Board
+{
+	public Board()
+	{
+		this.BillingTemplatesBoards = new HashSet<BillingTemplatesBoards>();
+	}
+
+	[Key]
+	public int Id { get; init; }
+
+	[Required]
+	[MaxLength(NameMaxLength, ErrorMessage = NameTooLongErrorMessage)]
+	public string Name { get; set; } = null!;
+
+	public virtual ICollection<BillingTemplatesBoards> BillingTemplatesBoards { get; set; } = null!;
+}
+
