36acfd9f25d2ffe1c105ed81dd42059f8e4dcdf3
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-07 08:24:52 +0200

    Created Currency entity model

---
+++ RevolutionPMS.Data.Models/Currency.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static RevolutionPMS.Common.EntitiesValidationsAndConstraints.Currency;
+
+namespace RevolutionPMS.Data.Models;
+
+public class Currency
+{
+	public Currency()
+	{
+		this.Id = Guid.NewGuid();
+		this.TaxSettings = new HashSet<TaxSettings>();
+	}
+
+	public Guid Id { get; set; }
+
+	[Required]
+	[MaxLength(NameMaxLength, ErrorMessage = NameTooLongErrorMessage)]
+	public string Name { get; set; } = null!;
+
+	public decimal ExchangeRate { get; set; }
+
+	public virtual ICollection<TaxSettings> TaxSettings { get; set; } = null!;
+}
+
