82f97062c8617b97eec1876dc51fd218b4cbec9d
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-07 08:24:08 +0200

    Created mapping model between BillingTemplate and Board models

---
+++ RevolutionPMS.Data.Models/BillingTemplatesBoards.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RevolutionPMS.Data.Models;
+
+public class BillingTemplatesBoards
+{
+	[Required]
+	public int BoardId { get; set; }
+
+	public virtual Board Board { get; set; } = null!;
+
+	[Required]
+	public Guid BillingTemplateId { get; set; }
+
+	public virtual BillingTemplate BillingTemplate { get; set; } = null!;
+}
+
