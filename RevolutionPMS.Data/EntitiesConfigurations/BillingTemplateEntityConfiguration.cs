e25babac1574504072871ee02117b6e480853649
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-09 10:49:57 +0200

    Created BillingTemplateEntity configuration class

---
+++ RevolutionPMS.Data/EntitiesConfigurations/BillingTemplateEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RevolutionPMS.Data.Models;
+
+namespace RevolutionPMS.Data.EntitiesConfigurations;
+
+public class BillingTemplateEntityConfiguration : IEntityTypeConfiguration<BillingTemplate>
+{
+    public void Configure(EntityTypeBuilder<BillingTemplate> builder)
+    {
+        builder
+            .Property(x => x.Price)
+            .HasColumnType("decimal(18,2)");
+
+        builder
+            .Property(x => x.TaxPersentage)
+            .HasColumnType("decimal(18,2)");
+    }
+}
+
