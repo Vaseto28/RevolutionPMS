75329191564ef3292b62534b6843a95d3d45c6f0
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-09 10:50:29 +0200

    Created Currency entity configuration class

---
+++ RevolutionPMS.Data/EntitiesConfigurations/CurrencyEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RevolutionPMS.Data.Models;
+
+namespace RevolutionPMS.Data.EntitiesConfigurations;
+
+public class CurrencyEntityConfiguration : IEntityTypeConfiguration<Currency>
+{
+    public void Configure(EntityTypeBuilder<Currency> builder)
+    {
+        builder
+            .Property(x => x.ExchangeRate)
+            .HasColumnType("decimal(18,2)");
+    }
+}
+
