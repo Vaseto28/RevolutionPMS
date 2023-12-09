88c164b9886d22e3bee70b0c635d204d5e26efef
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-09 10:51:08 +0200

    Created Tax entity configuration class

---
+++ RevolutionPMS.Data/EntitiesConfigurations/TaxSettingsEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RevolutionPMS.Data.Models;
+
+namespace RevolutionPMS.Data.EntitiesConfigurations
+{
+    public class TaxSettingsEntityConfiguration : IEntityTypeConfiguration<TaxSettings>
+	{
+        public void Configure(EntityTypeBuilder<TaxSettings> builder)
+        {
+            builder
+                .HasOne(x => x.DefaultCurrency)
+                .WithMany(x => x.TaxSettings)
+                .HasForeignKey(x => x.CurrencyId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Property(x => x.TaxPersentage)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
+
