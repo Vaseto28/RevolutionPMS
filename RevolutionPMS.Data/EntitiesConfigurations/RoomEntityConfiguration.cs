93088ee43f135010ec17daa2703829f4747ce8c9
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-09 10:50:51 +0200

    Created Room entity configuration class

---
+++ RevolutionPMS.Data/EntitiesConfigurations/RoomEntityConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RevolutionPMS.Data.Models;
+
+namespace RevolutionPMS.Data.EntitiesConfigurations;
+
+public class RoomEntityConfiguration : IEntityTypeConfiguration<Room>
+{
+    public void Configure(EntityTypeBuilder<Room> builder)
+    {
+        builder
+            .HasOne(x => x.RoomType)
+            .WithMany(x => x.Rooms)
+            .HasForeignKey(x => x.RoomTypeId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(x => x.Property)
+            .WithMany(x => x.Rooms)
+            .HasForeignKey(x => x.PropertyId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(x => x.Issue)
+            .WithMany(x => x.Rooms)
+            .HasForeignKey(x => x.IssueId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(x => x.HouseholdTask)
+            .WithMany(x => x.Rooms)
+            .HasForeignKey(x => x.HouseholdTaskId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
+
