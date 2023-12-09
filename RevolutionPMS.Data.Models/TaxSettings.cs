5ca8d52f91d3f8d986d5aae74c01999092ea87f3
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-09 10:49:11 +0200

    Created TaxSettings entity model

---
+++ RevolutionPMS.Data.Models/TaxSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static RevolutionPMS.Common.EntitiesValidationsAndConstraints.TaxSettings;
+
+namespace RevolutionPMS.Data.Models;
+
+public class TaxSettings
+{
+	[Key]
+	public Guid Id { get; set; }
+
+	[Required]
+	public bool IsTaxIncludedInThePrices { get; set; }
+
+	[Required]
+	[MaxLength(NameMaxLength, ErrorMessage = NameTooLongErrorMessage)]
+	public string TaxName { get; set; } = null!;
+
+	public decimal TaxPersentage { get; set; }
+
+	public int PriceRounding { get; set; }
+
+	[Required]
+	public Guid CurrencyId { get; set; }
+
+	public virtual Currency DefaultCurrency { get; set; } = null!;
+}
+
