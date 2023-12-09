70b0872990c83b08730acddc6e847e37832c6ab0
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-09 10:50:41 +0200

    Created Property entity configuration class

---
+++ RevolutionPMS.Data/EntitiesConfigurations/PropertyEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RevolutionPMS.Data.Models;
+
+namespace RevolutionPMS.Data.EntitiesConfigurations;
+
+public class PropertyEntityConfiguration : IEntityTypeConfiguration<Property>
+{
+    public void Configure(EntityTypeBuilder<Property> builder)
+    {
+        builder
+            .HasOne(x => x.PropertyType)
+            .WithMany(x => x.Properties)
+            .HasForeignKey(x => x.PropertyTypeId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(x => x.Company)
+            .WithMany(x => x.Properties)
+            .HasForeignKey(x => x.CompanyId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
+
