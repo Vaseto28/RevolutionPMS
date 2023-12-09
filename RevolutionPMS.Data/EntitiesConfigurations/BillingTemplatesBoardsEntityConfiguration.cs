8fad0763639b1ab1951ac07c27dee72f39d0ff46
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-09 10:50:17 +0200

    Created BillingTemplatesBoards entity configuration class

---
+++ RevolutionPMS.Data/EntitiesConfigurations/BillingTemplatesBoardsEntityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RevolutionPMS.Data.Models;
+
+namespace RevolutionPMS.Data.EntitiesConfigurations;
+
+public class BillingTemplatesBoardsEntityConfiguration : IEntityTypeConfiguration<BillingTemplatesBoards>
+{
+    public void Configure(EntityTypeBuilder<BillingTemplatesBoards> builder)
+    {
+        builder
+            .HasOne(x => x.BillingTemplate)
+            .WithMany(x => x.BillingTemplatesBoards)
+            .HasForeignKey(x => x.BillingTemplateId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(x => x.Board)
+            .WithMany(x => x.BillingTemplatesBoards)
+            .HasForeignKey(x => x.BoardId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasKey(x => new { x.BillingTemplateId, x.BoardId});
+    }
+}
+
