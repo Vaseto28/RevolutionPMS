5f201173905c2798f2d5b15fa69df3b2eb6f721e
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-12 11:41:52 +0200

    Updated RoomController class

--- RevolutionPMS.Web/Controllers/RoomController.cs
+++ RevolutionPMS.Web/Controllers/RoomController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace RevolutionPMS.Web.Controllers
 {
     public class RoomController : Controller
