d4bfb0c26bd211ae51d632231deefdcf57c0cacb
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-19 08:21:03 +0200

    Created BaseController class

---
+++ RevolutionPMS.Web/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RevolutionPMS.Web.Controllers;
+
+[Authorize]
+public class BaseController : Controller
+{
+
+}
+
