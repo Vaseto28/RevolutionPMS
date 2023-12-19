5a5e89cbbe7322a7a47504498305ba4bd91b5627
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-19 08:19:21 +0200

    Implemented SettingsService

---
+++ RevolutionPMS.Services/SettingsService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RevolutionPMS.Data;
+using RevolutionPMS.Data.Models;
+using RevolutionPMS.Services.Contracts;
+using RevolutionPMS.Web.ViewModels.SettingsModels;
+
+namespace RevolutionPMS.Services;
+
+public class SettingsService : ISettingsService
+{
+    private readonly RevolutionPMSDbContext dbCtx;
+
+    public SettingsService(RevolutionPMSDbContext dbCtx)
+    {
+        this.dbCtx = dbCtx;
+    }
+
+    public async Task AddPropertyTypeAsync(PropertyTypeViewModel model)
+    {
+        PropertyType propertyType = new PropertyType()
+        {
+            Name = model.Name
+        };
+
+        await this.dbCtx.PropertyTypes
+            .AddAsync(propertyType);
+
+        await this.dbCtx.SaveChangesAsync();
+    }
+
+    public async Task<ICollection<PropertyTypeViewModel>> GetAllPropertyTypesAsync()
+    {
+        return await this.dbCtx
+            .PropertyTypes
+            .Select(x => new PropertyTypeViewModel()
+            {
+                Name = x.Name
+            })
+            .ToArrayAsync();
+    }
+}
+
