bd74fd888cfe9d019df93a176d132b24595090a9
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-19 08:22:14 +0200

    Updated SettingsController class

--- RevolutionPMS.Web/Controllers/SettingsController.cs
+++ RevolutionPMS.Web/Controllers/SettingsController.cs
@@ -1,17 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RevolutionPMS.Services.Contracts;
+using RevolutionPMS.Web.ViewModels.SettingsModels;
 
 namespace RevolutionPMS.Web.Controllers
 {
     public class SettingsController : Controller
     {
+        private readonly ISettingsService settingsService;
+
+        public SettingsController(ISettingsService settingsService)
+        {
+            this.settingsService = settingsService;
+        }
+
         public IActionResult Menu()
         {
             return View();
         }
 
-        public IActionResult PropertyTypes()
+        [HttpGet]
+        public async Task<IActionResult> PropertyTypes()
         {
-            return View();
+            PropertyTypeViewModel propertyTypeViewModel = new PropertyTypeViewModel()
+            {
+                PropertyTypes = await this.settingsService.GetAllPropertyTypesAsync()
+            };
+
+            return View(propertyTypeViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PropertyTypes(PropertyTypeViewModel model)
+        {
+            await this.settingsService.AddPropertyTypeAsync(model);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
