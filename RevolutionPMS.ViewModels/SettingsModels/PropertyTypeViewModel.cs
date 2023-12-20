a3b7003fc3f5c59651ed2338fda39985d6558d0d
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-20 08:12:25 +0200

    Updated PropertyTypeViewModel

--- RevolutionPMS.ViewModels/SettingsModels/PropertyTypeViewModel.cs
+++ RevolutionPMS.ViewModels/SettingsModels/PropertyTypeViewModel.cs
@@ -8,6 +8,11 @@ public class PropertyTypeViewModel
 	[MinLength(NameMinLength, ErrorMessage = NameTooShortErrorMessage)]
 	public string Name { get; set; } = null!;
 
+	[Required]
+	public int AccommodationTypeNameId { get; set; }
+
+	public ICollection<AccommodationTypeViewModel> AccommodationTypeViewModels { get; set; } = null!;
+
 	public ICollection<PropertyTypeViewModel> PropertyTypes { get; set; } = null!;
 }
-
+;
