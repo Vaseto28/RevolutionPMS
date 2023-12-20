f9d14b988f4a993ef86989644b1d4bb5d7e07117
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-20 08:12:12 +0200

    Created AccommodationTypeViewModel class

---
+++ RevolutionPMS.ViewModels/SettingsModels/AccommodationTypeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static RevolutionPMS.Common.EntitiesValidationsAndConstraints.AccommodationType;
+
+namespace RevolutionPMS.Web.ViewModels.SettingsModels;
+
+public class AccommodationTypeViewModel
+{
+	public int Id { get; set; }
+
+	[Required]
+	[MinLength(NameMinLength, ErrorMessage = NameTooShortErrorMessage)]
+	public string Name { get; set; } = null!;
+}
+
