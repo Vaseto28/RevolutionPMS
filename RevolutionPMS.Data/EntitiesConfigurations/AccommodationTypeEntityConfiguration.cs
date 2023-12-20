b202c9666bc098b460173977dfb56bd8fb1e2940
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-20 08:11:43 +0200

    Created AccommodationTypeEntityConfiguration

---
+++ RevolutionPMS.Data/EntitiesConfigurations/AccommodationTypeEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RevolutionPMS.Data.Models;
+
+namespace RevolutionPMS.Data.EntitiesConfigurations;
+
+public class PropertyTypeEntityConfiguration : IEntityTypeConfiguration<PropertyType>
+{
+    public void Configure(EntityTypeBuilder<PropertyType> builder)
+    {
+        builder
+            .HasOne(x => x.AccommodationType)
+            .WithMany(x => x.PropertyTypes)
+            .HasForeignKey(x => x.AccommodationTypeId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
+
