b03cf0f3df6e15d66a8ff140637a417aa10bbb64
Vaseto28 <vasilkremenarovv@gmail.com>
2023-12-20 08:10:49 +0200

    Created AccommodationType entity model

---
+++ RevolutionPMS.Data.Models/AccommodationType.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static RevolutionPMS.Common.EntitiesValidationsAndConstraints.AccommodationType;
+
+namespace RevolutionPMS.Data.Models;
+
+public class AccommodationType
+{
+	public AccommodationType()
+	{
+		this.PropertyTypes = new HashSet<PropertyType>();
+	}
+
+	[Key]
+	public int Id { get; init; }
+
+	[Required]
+	[MaxLength(NameMaxLength, ErrorMessage = NameTooLongErrorMessage)]
+	public string Name { get; set; } = null!;
+
+	public virtual ICollection<PropertyType> PropertyTypes { get; set; } = null!;
+}
+
